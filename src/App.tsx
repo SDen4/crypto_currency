ee311ad87c50480ca6c04af5639383fb5ff8f3bf
SDen4 <asw325@yandex.ru>
2022-05-02 18:39:53 +0500

    adjustable height

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import AppWs from './components/AppWs';
 
-import './App.css';
+import styles from './App.module.css';
 
 function App() {
+  const [appHeight, setAppHeight] = useState<number>(0);
+  useEffect(() => setAppHeight(window.innerHeight), []);
+
+  // resize
+  window.addEventListener(
+    'resize',
+    () => setAppHeight(window.innerHeight),
+    true,
+  );
+
   return (
-    <div className="App">
+    <div className={styles.App} style={{ minHeight: appHeight }}>
       <header className="App-header">
         <h1>Crypto currency</h1>
       </header>
