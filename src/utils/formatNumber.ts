4616a7c57b088c9fb07694d81c298fc55cef51c2
SDen4 <asw325@yandex.ru>
2022-09-09 21:18:09 +0500

    docs: format numbers func

--- src/utils/formatNumber.ts
+++ src/utils/formatNumber.ts
@@ -1,6 +1,12 @@
+/**
+ * Formating a number with the specified rounding, or by default - to an integer.
+ * If necessary, you can add a symbol.
+ */
+
 export const formatNumber = (number: number, max?: number, symbol?: string) => {
   const formattedNumber = new Intl.NumberFormat('ru-RU', {
     maximumFractionDigits: max || 0,
   }).format(number);
+
   return `${formattedNumber}${symbol || ''}`;
 };
