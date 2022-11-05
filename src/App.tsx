d0161c089083476b7fe9a6cde9fc2417f8d7c963
SDen4 <asw325@yandex.ru>
2022-11-05 23:34:02 +0500

    cors

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
 
   useEffect(() => {
     // statistic (test)
-    const data = stat();
+    stat();
 
     setAppHeight(window.innerHeight);
   }, []);
--- src/utils/stat.ts
+++ src/utils/stat.ts
@@ -1,7 +1,15 @@
-export const stat = async () => {
-  const data = fetch(
-    'http://ovz1.j34847422.m61kn.vps.myjino.ru?test=test',
-  ).then((res) => res.json());
+const invocation = new XMLHttpRequest();
+const url = 'http://ovz1.j34847422.m61kn.vps.myjino.ru?test=test';
 
-  return data;
+export const stat = async () => {
+  // const data = fetch(
+  //   'http://ovz1.j34847422.m61kn.vps.myjino.ru?test=test',
+  // ).then((res) => res.json());
+
+  // return data;
+  if (invocation) {
+    invocation.open('GET', url, true);
+    // invocation.onreadystatechange = handler;
+    invocation.send();
+  }
 };
