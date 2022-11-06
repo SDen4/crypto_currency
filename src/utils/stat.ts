7d3bc4143e0a7351d3905914a03a4f6acca18d9b
SDen4 <asw325@yandex.ru>
2022-11-06 11:14:48 +0500

    cors

--- src/utils/stat.ts
+++ src/utils/stat.ts
@@ -1,15 +1,21 @@
-const invocation = new XMLHttpRequest();
-const url = 'http://ovz1.j34847422.m61kn.vps.myjino.ru?test=test';
-
 export const stat = async () => {
-  // const data = fetch(
-  //   'http://ovz1.j34847422.m61kn.vps.myjino.ru?test=test',
-  // ).then((res) => res.json());
+  const params: RequestInit = {
+    method: 'GET', // *GET, POST, PUT, DELETE, etc.
+    mode: 'no-cors', // no-cors, *cors, same-origin
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+      'Content-Type': 'application/json',
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: 'follow', // manual, *follow, error
+    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+  };
 
-  // return data;
-  if (invocation) {
-    invocation.open('GET', url, true);
-    // invocation.onreadystatechange = handler;
-    invocation.send();
-  }
+  const data = fetch(
+    'http://ovz1.j34847422.m61kn.vps.myjino.ru?test=test',
+    params,
+  ).then((res) => res.json());
+
+  return data;
 };
