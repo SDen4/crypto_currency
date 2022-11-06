eb604379c4e2d346c8fc32b95edbb8a71b257c83
SDen4 <asw325@yandex.ru>
2022-11-06 12:34:36 +0500

    refactor: cors

--- src/utils/stat.ts
+++ src/utils/stat.ts
@@ -1,7 +1,7 @@
 export const stat = async () => {
   const params: RequestInit = {
     method: 'GET', // *GET, POST, PUT, DELETE, etc.
-    mode: 'no-cors', // no-cors, *cors, same-origin
+    mode: 'cors', // no-cors, *cors, same-origin
     cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
     // credentials: 'same-origin', // include, *same-origin, omit
     headers: {
